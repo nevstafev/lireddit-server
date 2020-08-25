ca8fe5193a735e68af0df6a034c12a6e8c6eeeff
nevstafev <evstafev.n.e@gmail.com>
2020-08-25 09:37:21 +0300

    fix errors

--- src/resolvers/post.ts
+++ src/resolvers/post.ts
@@ -1,6 +1,6 @@
-import { Resolver, Query, Ctx, Arg, Int, Mutation } from 'type-graphql';
+import { Resolver, Query, Ctx, Arg, Mutation } from 'type-graphql';
 import { Post } from '../entities/Post';
-import { MyContext } from 'src/types/types';
+import { MyContext } from '../types/types';
 
 @Resolver()
 export class PostResolver {
