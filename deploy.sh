d2bd88a6c620309b783fc12f9eabaf07b0e4faf2
nevstafev <evstafev.n.e@gmail.com>
2020-11-08 08:36:11 +0300

    fix deploy

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 echo enter version for tagging
 read VERSION
 
-docker build -t nevstafev/lireddit:$VERSION
+docker build -t nevstafev/lireddit:$VERSION .
 docker push nevstafev/lireddit:$VERSION
 
 ssh root@165.227.167.2 "docker pull nevstafev/lireddit:$VERSION && docker tag nevstafev/lireddit:$VERSION dokku/api:$VERSION && dokku deploy api:$VERSION"
